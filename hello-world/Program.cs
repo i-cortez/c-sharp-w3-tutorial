c90b775a858ca4ced8aa7f4e2e2db9208a3c9217
Izze <56184561+i-cortez@users.noreply.github.com>
2020-10-21 17:14:50 -0700

    Tutorial file is up

---
+++ hello-world/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hello_world
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // WriteLine() prints the output on a new line each time
+         Console.WriteLine("Hello World!");
+         Console.WriteLine("I will print on a new line");
+
+         // Write() prints on the same line
+         Console.Write("Hello World! ");
+         Console.Write("I will print on the same line.");
+      }
+   }
+}
+
