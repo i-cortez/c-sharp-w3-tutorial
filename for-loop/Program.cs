ef71b8cdf273fc0b15d7564d42bee4319e805ba4
Izze <56184561+i-cortez@users.noreply.github.com>
2020-10-22 16:45:24 -0700

    Adding C# examples of loops

---
+++ for-loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace for_loop
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // when you know exactly how many times you want to loop through a block of code, use the for loop
+         for(int i = 0; i < 5; ++i)
+         {
+            Console.WriteLine(i);
+         }
+
+         // print the even values between 0 and 10
+         for (int i = 0; i <= 10; i += 2)
+         {
+            Console.WriteLine(i);
+         }
+
+         // the foreach loop is used exclusively to loop through elements in an array
+         string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+         foreach(string i in cars)
+         {
+            Console.WriteLine(i);
+         }
+      }
+   }
+}
+
---
+++ while-loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace while_loop
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // loops can execute a block of code as long as a specified condition is reached
+         // the while loop loops through a block of code as long as a specified condition is true
+         int i = 0;
+         while(i < 5)
+         {
+            Console.WriteLine(i);
+            ++i; // do not forget to increase the variable used in the condition
+         }
+
+         // the do while loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true
+         i = 0;
+         do
+         {
+            Console.WriteLine(i);
+            ++i;
+         }
+         while (i < 5);
+      }
+   }
+}
+
