8552d567cc89e80f5d6ba290b54f05d3f7ee76f8
Izze <56184561+i-cortez@users.noreply.github.com>
2020-10-21 22:15:22 -0700

    Adding a C# example of Booleans

---
+++ booleans/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace booleans
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // C# has a bool data type, which can take the values true or false
+
+         // a boolean type is declared with the bool keyword
+         bool isCSharpFun = true;
+         bool isFishTasty = false;
+         Console.WriteLine(isCSharpFun);
+         Console.WriteLine(isFishTasty);
+
+         // a boolean expression is a C# expression that returns a Boolean value
+         int x = 10, y = 9;
+         Console.WriteLine(x > y);
+         Console.WriteLine(x == 9);
+      }
+   }
+}
+
+
