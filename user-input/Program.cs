e543a9f8f12072b25a08165b7b34d7e74aebe5ef
Izze <56184561+i-cortez@users.noreply.github.com>
2020-10-21 20:58:52 -0700

    New example from tutorial

---
+++ user-input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace user_input
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // use Console.ReadLine() to get user input
+         // type your username and press enter
+         Console.WriteLine("Enter username: ");
+
+         // create a string variable and get user input from the keyboard
+         string userName = Console.ReadLine();
+
+         // output the obtained value
+         Console.WriteLine("Username is: " + userName);
+
+         // you cannot implicitly convert types with Console.ReadLine()
+         Console.WriteLine("Enter your age: ");
+         int age = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Your age is: " + age);
+      }
+   }
+}
